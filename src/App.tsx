ab31278e4931d2c7096301884a8e11ef14aeb70f
Farkhod Kodirov <farxod94@gmail.com>
2023-08-15 01:05:27 +0500

    feat: implement modifiendData to change name using 3 types of errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,100 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
 import faker from "faker";
-
+import seedrandom from "seedrandom";
+const alphabets = {
+  en: "abcdefghijklmnopqrstuvwxyz",
+  pl: "aąbcćdeęfghijklłmnńoóprsśtuwyzźż",
+  fn: "abcdefghijklmnopqrstuvwxyzåäö",
+};
 function App() {
   const [data, setData] = useState<any[]>([]);
+  const [changedData, setChangedData] = useState<any[]>([]);
   const [region, setRegion] = useState<string>("en");
   const [seed, setSeed] = useState<string>("0");
   const [errorCount, setErrorCount] = useState<number>(0);
+  interface Idata {
+    id: string;
+    name: string;
+    address: string;
+    phone: string;
+  }
+  useEffect(() => {
+    console.log(999);
+    generateData();
+  }, [region, seed]);
 
   useEffect(() => {
-    generateData();
-  }, [region, seed, errorCount]);
+    if (data.length > 0) {
+      console.log(777);
+      modifiedData();
+    }
+  }, [errorCount, data]);
 
   const generateData = () => {
     faker.locale = region;
     faker.seed(parseInt(seed));
-
-    const newData = [];
+    const data = [];
 
     for (let i = 0; i < 20; i++) {
       const id = faker.datatype.uuid();
       const name = faker.name.findName();
       const address = faker.address.streetAddress();
       const phone = faker.phone.phoneNumberFormat();
-      newData.push({ id, name, address, phone });
+      data.push({ id, name, address, phone });
     }
-
-    setData(newData);
+    setData(data);
   };
+
+  const modifiedData = () => {
+    const newData = [];
+    data.forEach((item: Idata, index) => {
+      const randomGenerator = seedrandom(seed);
+      let { id, name, address, phone } = item;
+      const qwerty =
+        errorCount - Math.floor(errorCount) === 0.5
+          ? Math.floor(errorCount) + (Math.floor(name.length) % 2)
+          : Math.round(errorCount);
+      for (let j = 0; j < qwerty; j++) {
+        console.log(qwerty);
+        const errorType = ["insert", "delete", "swap"];
+        // const errorIndex = Math.floor(randomGenerator() * 3);
+        // const qw = (randomGenerator() * name.length) % 3;
+
+        const errorIndex = Math.floor(randomGenerator() * name.length) % 3;
+        const randomIndex = Math.floor(randomGenerator() * name.length);
+        // console.log(`${errorIndex}`);
+
+        if (errorType[errorIndex] === "insert") {
+          // console.log(`${randomIndex}-${randomGenerator()}`);
+          // console.log(`${name}---${index}`);
+          // console.log(`${randomIndex}`);
+          const newRandom = seedrandom(name);
+          const getAlphabet = alphabets.en;
+          const newIndex = Math.floor(newRandom() * getAlphabet.length);
+          const randomLetter = getAlphabet[newIndex];
+          // console.log(`${name}`);
+          //  console.log(`${name}-${randomLetter}-${randomIndex}`);
+          name =
+            name.slice(0, randomIndex) + randomLetter + name.slice(randomIndex);
+          // console.log(`${name}-${randomLetter}-${randomIndex}`);
+        } else if (errorType[errorIndex] === "delete") {
+          name = name.slice(0, randomIndex) + name.slice(randomIndex + 1);
+        } else if (errorType[errorIndex] === "swap") {
+          const nextIndex = randomIndex + 1;
+          name =
+            name.slice(0, randomIndex) +
+            name[nextIndex] +
+            name[randomIndex] +
+            name.slice(nextIndex + 1);
+        }
+      }
+      newData.push({ id, name, address, phone });
+    });
+    // console.log(newData);
+    setChangedData(newData);
+  };
+  const displayedData = errorCount === 0 ? data : changedData;
   return (
     <div className="container">
       <h1>Fake Data Generator</h1>
@@ -47,9 +114,12 @@ function App() {
         <label>Error Count:</label>
         <input
           type="number"
+          step="0.1"
           className="form-control"
           value={errorCount}
-          onChange={(e) => setErrorCount(parseInt(e.target.value))}
+          onChange={(e) => {
+            setErrorCount(+e.target.value);
+          }}
         />
       </div>
       <div className="form-group">
@@ -75,7 +145,7 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {data.map((item, index) => (
+          {displayedData.map((item, index) => (
             <tr key={item.id}>
               <td>{index + 1}</td>
               <td>{item.id}</td>
